2d63e4621bf456a680f6b6bb898e25d9439adab2
Chinta Pranay <chinta.pranay@factset.com>
2020-05-05 22:04:38 +0530

    Day 5 First Unique character in string

---
+++ src/may/challenge/FirstUniqueChar.java
@@ -0,0 +1,21 @@
+package may.challenge;
+
+public class FirstUniqueChar {
+	public static void main(String[] args) {
+		FirstUniqueChar firstUniqueChar = new FirstUniqueChar();
+		System.out.println(firstUniqueChar.firstUniqChar(""));
+	}
+    public int firstUniqChar(String s) {
+    	int uniqueIndex = -1;
+    	int[] uniq = new int[26];
+    	for(int i = s.length()-1 ; i >= 0;i--) {
+    		uniq[ s.charAt(i) -'a']++;
+    	}
+    	for(int i = 0;i<s.length();i++) {
+    		if(uniq[ s.charAt(i) -'a'] == 1) {
+    			return i;
+    		}
+    	}
+    	return uniqueIndex;
+    }
+}
